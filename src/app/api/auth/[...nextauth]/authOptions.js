f8e32cb8715bac8a1f54c706a48bb6d8220f37a3
Satoru Nakamura <na.kamura.1263@gmail.com>
2024-11-15 15:00:12 +0900

    fix: bug

--- src/app/api/auth/[...nextauth]/authOptions.js
+++ src/app/api/auth/[...nextauth]/authOptions.js
@@ -1,4 +1,5 @@
 export const authOptions = {
+  debug: true, // next-auth のデバッグモードを有効化
   providers: [
     {
       id: "gakunin",
@@ -9,9 +10,9 @@ export const authOptions = {
       authorization: {
         url: "https://accounts.rdm.nii.ac.jp/oauth2/authorize",
         params: {
-          scope: "osf.full_read osf.full_write",
+          scope: process.env.OSF_SCOPE || "osf.full_read osf.full_write", // 環境変数でスコープを管理
           response_type: "code",
-          redirect_uri: `${process.env.NEXTAUTH_URL}/api/auth/callback/gakunin`,
+          redirect_uri: `${process.env.NEXTAUTH_URL}/api/auth/callback/gakunin`, // 環境変数からリダイレクトURIを構築
         },
       },
       token: "https://accounts.rdm.nii.ac.jp/oauth2/token",
@@ -24,32 +25,44 @@ export const authOptions = {
               Accept: "application/json",
             },
           });
+          console.log("aaaaaaaa")
           return await res.json();
         },
       },
       profile(profile) {
         return {
-          id: profile.id, // GakuNin RDM のユーザー ID
-          name: profile.full_name,
-          email: profile.email,
+          id: profile.data.id, // GakuNin RDM のユーザー ID
+          name: profile.data.attributes.full_name, // attributesの中からfull_nameを取得
+          email: profile.data.attributes.email,    // attributesの中からemailを取得
         };
-      },
+      }
     },
   ],
   callbacks: {
     async session({ session, token }) {
       session.accessToken = token.accessToken;
-      session.user.id = token.id; // GakuNin ID をセッションに追加
+      session.refreshToken = token.refreshToken; // リフレッシュトークンをセッションに保存
+      session.user.id = token.id; // osf ID をセッションに追加
       return session;
     },
     async jwt({ token, account, user }) {
       if (account) {
         token.accessToken = account.access_token;
+        token.refreshToken = account.refresh_token; // リフレッシュトークンを保存
       }
       if (user) {
         token.id = user.id; // ユーザー ID をトークンに保存
       }
       return token;
+    },
+  },
+  events: {
+    async signIn({ user, account }) {
+      console.log("User signed in:", user);
+      console.log("Account details:", account);
+    },
+    async error({ error }) {
+      console.error("An error occurred during the authentication process:", error);
     },
   },
 };
